b086e80f27d4f0444d21e31aedc1cc67f369901b
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2020-04-05 20:49:41 +0300

    Добавлен класс Converter и функции в него

--- Task 1/Program.cs
+++ Task 1/Program.cs
@@ -7,11 +7,61 @@ using System;
 
 namespace Task_1
 {
+    class Converter
+    {
+        public enum Сurrency
+        {
+            USD,
+            EUR,
+            RUB
+        }
+        private double usdCourse = 0.0;
+        private double eurCourse = 0.0;
+        private double rubCourse = 0.0;
+        public double tjsCourse { get; set; }
+        public Converter(double _usd, double _eur, double _rub)
+        {
+            usdCourse = _usd;
+            eurCourse = _eur;
+            rubCourse = _rub;
+        }
+
+        public double ConvertToTJS(double _tjs, Сurrency currency)
+        {
+            switch (currency)
+            {
+                case Сurrency.EUR:
+                return _tjs * eurCourse;
+                case Сurrency.RUB:
+                return _tjs * rubCourse;
+                case Сurrency.USD:
+                return _tjs * usdCourse;
+                default:
+                return 0.0f;
+            }
+        }
+
+        public double ConvertTJSTo(double _maney, Сurrency currency)
+        {
+            switch (currency)
+            {
+                case Сurrency.EUR:
+                return _maney * tjsCourse;
+                case Сurrency.RUB:
+                return _maney * tjsCourse;
+                case Сurrency.USD:
+                return _maney * tjsCourse;
+                default:
+                return 0.0f;
+            }
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
