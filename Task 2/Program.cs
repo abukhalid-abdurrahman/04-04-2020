b5cc18fe3e5e9d2088e16108dd73283dd74d3640
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2020-04-05 22:05:48 +0300

    Добавлены поля и конструктор

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -9,7 +9,15 @@ namespace Task_2
 {
     class Employee
     {
-        
+        public string Name { get; set; } 
+        public string Surname { get; set; }
+        public string Position { get; set; }
+        public string Salary { get; set; }
+        public Employee(string _name, string _surname)
+        {
+            Name = _name;
+            Surname = _surname;
+        }
     }
     class Program
     {
