069c7fb3aeedb8ffdb94c308652d412e58702104
faha-berdiev <madysoftware@gmail.com>
2020-04-05 22:14:23 +0300

    Начало решений задач

---
+++ Task 1/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Developed by Faridun Berdiev
+ * Date: 04.04.2020
+ * First Task
+*/
+using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Task 2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+ * Developed by Faridun Berdiev
+ * Date: 04.04.2020
+ * Second Task
+*/
+using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
