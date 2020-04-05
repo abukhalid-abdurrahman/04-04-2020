44290494d6d39db75ee60eca67c8e9d3ddb3015c
faha-berdiev <62469844+faha-berdiev@users.noreply.github.com>
2020-04-05 22:43:15 +0300

    Не большие изменения

--- Task 2/Program.cs
+++ Task 2/Program.cs
@@ -9,14 +9,50 @@ namespace Task_2
 {
     class Employee
     {
+        public enum Positions
+        {
+            Supervisor = 8000,
+            Manager = 6000,
+            Programmer = 5500,
+            Cleaner = 2000
+        }
+        ///<summary>
+        ///Имя сотрудника
+        ///</summary>
         public string Name { get; set; } 
+        ///<summary>
+        ///Фамилия сотрудника
+        ///</summary>
         public string Surname { get; set; }
-        public string Position { get; set; }
+        ///<summary>
+        ///Должность сотрудника
+        ///</summary>
+        public Positions EmployeePosition { get; set; }
+        ///<summary>
+        ///Зар-плата сотрудника
+        ///</summary>
         public string Salary { get; set; }
+        ///<summary>
+        ///Стаж работы сотрудника
+        ///</summary>
+        public int Experience { get; set; }
         public Employee(string _name, string _surname)
         {
             Name = _name;
             Surname = _surname;
+        }
+
+        public double CalculateSalary()
+        {
+            double results = 0.0f;
+            if(Experience > 5)
+                results += 300;
+            else if(Experience > 10)
+                results += 600;
+            else if(Experience > 15)
+                results += 2200;
+
+            return results;
         }
     }
     class Program
